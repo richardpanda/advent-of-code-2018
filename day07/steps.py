5deda07f59ab535972ecca7ef45e23fe6900551c
Richard Ho <richardpanda1993@gmail.com>
2018-12-06 23:57:01 -0800

    Remove time from available_steps

--- day07/steps.py
+++ day07/steps.py
@@ -32,20 +32,20 @@ def calculate_completion_time(steps, step_duration, num_workers):
         step_to_next_steps[u].append(v)
         steps_to_num_requirements[v] += 1
     available_steps = [
-        (step, 0) for step in step_to_next_steps if steps_to_num_requirements[step] == 0
+        step for step in step_to_next_steps if steps_to_num_requirements[step] == 0
     ]
     heapify(available_steps)
     processing_steps, completion_time = [], 0
     while available_steps or processing_steps:
         while available_steps and len(processing_steps) < num_workers:
-            step, start_time = heappop(available_steps)
+            step = heappop(available_steps)
             end_time = completion_time + ord(step) - 64 + step_duration
             heappush(processing_steps, (end_time, step))
         completion_time, step = heappop(processing_steps)
         for next_step in step_to_next_steps[step]:
             steps_to_num_requirements[next_step] -= 1
             if steps_to_num_requirements[next_step] == 0:
-                heappush(available_steps, (next_step, end_time))
+                heappush(available_steps, next_step)
     return completion_time
 
 
