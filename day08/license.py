4e2d7cb2775fad4e16f659d2ecaddf4163b6e95e
Richard Ho <richardpanda1993@gmail.com>
2018-12-08 10:45:12 -0800

    Replace pointer with stack

--- day08/license.py
+++ day08/license.py
@@ -1,45 +1,29 @@
 def calculate_metadata_sum(license):
     def calculate_metadata_sum_dfs():
-        nonlocal idx
-        num_children, num_metadata = license[idx : idx + 2]
-        idx += 2
+        num_children, num_metadata = license.pop(), license.pop()
+        return sum(calculate_metadata_sum_dfs() for _ in range(num_children)) + sum(
+            license.pop() for _ in range(num_metadata)
+        )
 
-        if num_children == 0:
-            metadata_sum = sum(license[idx : idx + num_metadata])
-            idx += num_metadata
-            return metadata_sum
-
-        metadata_sum = sum(calculate_metadata_sum_dfs() for _ in range(num_children))
-        metadata_sum += sum(license[idx : idx + num_metadata])
-        idx += num_metadata
-
-        return metadata_sum
-
-    idx = 0
+    license = list(reversed(license))
     return calculate_metadata_sum_dfs()
 
 
 def calculate_root_value(license):
     def calculate_root_value_dfs():
-        nonlocal idx
-        num_children, num_metadata = license[idx : idx + 2]
-        idx += 2
-
-        if num_children == 0:
-            metadata_sum = sum(license[idx : idx + num_metadata])
-            idx += num_metadata
-            return metadata_sum
-
+        num_children, num_metadata = license.pop(), license.pop()
         child_values = [calculate_root_value_dfs() for _ in range(num_children)]
-        metadata = license[idx : idx + num_metadata]
-        idx += num_metadata
-
-        return sum(
-            child_values[entry - 1] if 1 <= entry <= len(child_values) else 0
-            for entry in metadata
+        metadata = [license.pop() for _ in range(num_metadata)]
+        return (
+            sum(
+                child_values[entry - 1] if 1 <= entry <= len(child_values) else 0
+                for entry in metadata
+            )
+            if num_children
+            else sum(metadata)
         )
 
-    idx = 0
+    license = list(reversed(license))
     return calculate_root_value_dfs()
 
 
