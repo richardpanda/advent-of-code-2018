1823f15fa17c7f7ac7d0ef6ad520e7b5b14bc0e1
Richard Ho <richardpanda1993@gmail.com>
2018-12-09 12:15:00 -0800

    Use deque

--- day09/marble.py
+++ day09/marble.py
@@ -1,35 +1,20 @@
-class CircularDoublyLinkedListNode:
-    def __init__(self, value, prev=None, _next=None):
-        self.value = value
-        self.prev = prev
-        self.next = _next
+from collections import deque
 
 
 def calculate_high_score(num_players, last_marble_points):
     scores = [0] * num_players
-
-    curr_node = CircularDoublyLinkedListNode(0)
-    curr_node.prev = curr_node.next = curr_node
+    circle = deque([0])
 
     for marble in range(1, last_marble_points + 1):
         player = (marble - 1) % num_players
 
         if marble % 23 == 0:
-            for _ in range(7):
-                curr_node = curr_node.prev
-
-            scores[player] += marble + curr_node.value
-            curr_node.prev.next, curr_node.next.prev, curr_node = (
-                curr_node.next,
-                curr_node.prev,
-                curr_node.next,
-            )
+            circle.rotate(7)
+            scores[player] += marble + circle.pop()
+            circle.rotate(-1)
         else:
-            curr_node = curr_node.next
-            curr_node.next = CircularDoublyLinkedListNode(
-                marble, curr_node, curr_node.next
-            )
-            curr_node.next.next.prev, curr_node = curr_node.next, curr_node.next
+            circle.rotate(-1)
+            circle.append(marble)
 
     return max(scores)
 
